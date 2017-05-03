ca6b620688143c7129c54278a37273a1865eb854
Lukas <lpagitz@gmail.com>
2017-05-03 02:22:17 +0200

    some more refactoring

--- app/src/main/java/com/example/wenboda/sk_ui3/EndScreen.java
+++ app/src/main/java/com/example/wenboda/sk_ui3/EndScreen.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-
 public class EndScreen extends AppCompatActivity {
     Button button;
     TextView who_is_starting;
@@ -18,18 +17,18 @@ public class EndScreen extends AppCompatActivity {
         setContentView(R.layout.activity_end_screen);
 
         button = (Button) findViewById(R.id.button_gotoStart);
-        who_is_starting=(TextView) findViewById(R.id.text_first);
+        who_is_starting = (TextView) findViewById(R.id.text_first);
 
-        Intent intent= getIntent();
-        if(intent.getExtras().getBoolean("who_is_starting")){
+        Intent intent = getIntent();
+        if (intent.getExtras().getBoolean("who_is_starting")) {
             who_is_starting.setText("You are first");
-        }else{
+        } else {
             who_is_starting.setText("Enemy is first or tie");
         }
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent nextScreen =  new Intent(getApplicationContext(),Map.class);
+                Intent nextScreen = new Intent(getApplicationContext(), Map.class);
                 startActivity(nextScreen);
             }
         });
