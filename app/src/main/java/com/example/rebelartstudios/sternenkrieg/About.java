8c7f47a1fdd2223579b487030cfb1e65c8057dcb
Christian Bauer <chrbauer@edu.aau.at>
2017-06-14 14:39:40 +0200

    Added id to some XML files, corrected typo, started on ShipLogic class to make Unit testing easier

--- app/src/main/java/com/example/rebelartstudios/sternenkrieg/About.java
+++ app/src/main/java/com/example/rebelartstudios/sternenkrieg/About.java
@@ -4,8 +4,8 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.media.MediaPlayer;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -36,5 +36,6 @@ public class About extends AppCompatActivity {
                 }
             }
         });
+
     }
 }
