a8bdf6764cff032f94115298851df931379db04c
Roither <christoph.roither@gmail.com>
2017-06-21 14:38:41 +0200

    alte ip

--- app/src/test/java/com/example/rebelartstudios/sternenkrieg/NetworkingUnitTests.java
+++ app/src/test/java/com/example/rebelartstudios/sternenkrieg/NetworkingUnitTests.java
@@ -40,7 +40,7 @@ public class NetworkingUnitTests {
     public void setUp(){
         socketH = new Socket();
         socketC = new Socket();
-        String ip = System.getProperty("myapplication.ip");
+        ip = "10.0.2.2";
         acceptThread  = new AcceptThread(running,serverSocket,socketH,handlerH,receiveThreadHost,12345);
         startThread = new StartThread(socketC,ip,receiveThreadClient,handlerC,12345);
         acceptThread.start();
